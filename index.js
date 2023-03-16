c6a48904049876a1e0ac1a299a7d0678b7958fcc
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-03-16 01:49:06 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(string){
   // Write your code here!
+  if (string === 199) {
+    return "This one is on me!";
+  }
+  else if (string === 1500){
+    return "That will be twenty bucks."
+  }
+  else if (string === 2001){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (string === 2501){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(string){
   // Write your code here!
+  return string==='NYC'? 'Ok, sounds good.' : 'No go.';
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(string){
   // Write your code here!
+  switch (string) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+      break;
+  }
 }
